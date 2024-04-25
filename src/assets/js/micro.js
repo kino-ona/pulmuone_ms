2322a08597bda47ac8dda480cd029a67a456cea7
ona-yoonhee <167725427+ona-yoonhee@users.noreply.github.com>
2024-04-25 19:34:16 +0900

    0425 작업

--- src/assets/js/micro.js
+++ src/assets/js/micro.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const archiveSwiper = new Swiper(".archive__swiper", {
+	autoplay: {
+		delay: 0,
+		disableOnInteraction: false
+	},
+	speed: 6000,
+	loop: true,
+	slidesPerView: "auto",
+	spaceBetween: 20,
+	freeMode: true,
+	allowTouchMove: false
+
+	// breakpoints: {
+	// 	1280: {
+	//
+	// 	}
+	// }
+});
+
+//아카이브 슬라이드
+const archiveArea = document.querySelector(".archive_box");
+
+archiveArea.addEventListener("mouseenter", () => {
+	archiveSwiper.stop();
+});
+
+archiveArea.addEventListener("mouseleave", () => {
+	archiveSwiper.start();
+});
