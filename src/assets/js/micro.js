9f3fa107904e043903f5d6f28786fc853a85b36f
ona-yoonhee <167725427+ona-yoonhee@users.noreply.github.com>
2024-04-26 16:50:24 +0900

    0426 작업

--- src/assets/js/micro.js
+++ src/assets/js/micro.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const archiveSwiper = new Swiper(".archive_swiper", {
+const archiveSwiper = new Swiper(".archive__swiper", {
 	autoplay: {
 		delay: 0,
-		disableOnInteraction: false
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true
 	},
 	speed: 6000,
 	loop: true,
@@ -21,18 +22,18 @@ const archiveSwiper = new Swiper(".archive_swiper", {
 //아카이브 슬라이드
 const archiveArea = document.querySelector(".archive_box");
 
-archiveArea.addEventListener("mouseenter", () => {
-	//let activeSlide = document.querySelector(".swiper-slide-active").dataset.swiperSlideIndex;
-	//archiveSwiper.slideTo(activeSlide);
-	archiveSwiper.autoplay.stop();
-});
+// archiveArea.addEventListener("mouseenter", () => {
+// 	//let activeSlide = document.querySelector(".swiper-slide-active").dataset.swiperSlideIndex;
+// 	//archiveSwiper.slideTo(activeSlide);
+// 	archiveSwiper.autoplay.stop();
+// });
 
-archiveArea.addEventListener("mouseleave", () => {
-	archiveSwiper.autoplay.start();
-});
+// archiveArea.addEventListener("mouseleave", () => {
+// 	archiveSwiper.autoplay.start();
+// });
 
 //lottie
-var animation1 = bodymovin.loadAnimation({
+let animation1 = bodymovin.loadAnimation({
 	container: document.getElementById("lottie_1"), // Required
 	path: "https://assets5.lottiefiles.com/packages/lf20_ngcpf3x7.json", //path: 'data.json', // 실제 사용 폴더 지정 ex) data.json
 	renderer: "svg", // Required
@@ -40,7 +41,7 @@ var animation1 = bodymovin.loadAnimation({
 	autoplay: true // Optional
 });
 
-var animation2 = bodymovin.loadAnimation({
+let animation2 = bodymovin.loadAnimation({
 	container: document.getElementById("lottie_2"), // Required
 	path: "https://assets1.lottiefiles.com/packages/lf20_yoltywwd.json", //path: 'data.json', // 실제 사용 폴더 지정 ex) data.json
 	renderer: "svg", // Required
