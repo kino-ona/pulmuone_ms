6904900447b66eee91aafdb7fc2ef7d3db1116e0
ona-yoonhee <167725427+ona-yoonhee@users.noreply.github.com>
2024-05-02 17:54:24 +0900

    0502 작업

--- src/assets/js/micro.js
+++ src/assets/js/micro.js
@@ -1,46 +1,13 @@
 "use strict";
 
 //archive swiper
-const archiveBox = document.querySelector(".archive__pcBox");
-const archiveContainers = document.querySelectorAll(".archive__swiper");
-const archiveList = [];
-
-archiveContainers.forEach((container, index) => {
-	const archiveSwiper = new Swiper(container, {
-		autoplay: {
-			delay: 0,
-			disableOnInteraction: false
-			//pauseOnMouseEnter: true
-		},
-		speed: 5000,
-		loop: true,
-		slidesPerView: "auto",
-		spaceBetween: 20,
-		allowTouchMove: false
-	});
-	archiveList.push(archiveSwiper);
-});
-
-function stopAllSwipers() {
-	archiveList.forEach((archiveSwiper) => {
-		archiveSwiper.autoplay.stop();
-	});
-}
-
-function startAllSwipers() {
-	archiveList.forEach((archiveSwiper) => {
-		archiveSwiper.autoplay.start();
-	});
-}
-
-archiveBox.addEventListener("mouseenter", () => {
-	archiveContainers.forEach((container) => {
-		stopAllSwipers();
-	});
-});
-
-archiveBox.addEventListener("mouseleave", () => {
-	archiveContainers.forEach((container) => {
-		startAllSwipers();
-	});
+const swiper = new Swiper(".archive__swiper", {
+	autoplay: {
+		delay: 0,
+		desableOnInteraction: false
+	},
+	speed: 8000,
+	loop: true,
+	slidesPerView: "auto",
+	freemode: true
 });
