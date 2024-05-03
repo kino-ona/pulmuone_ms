327d50acfb0f204588859b593220d19cc79d644a
ona-yoonhee <167725427+ona-yoonhee@users.noreply.github.com>
2024-05-03 20:09:18 +0900

    0503 작업 최종

--- src/assets/js/micro.js
+++ src/assets/js/micro.js
@@ -1,13 +1,1 @@
 "use strict";
-
-//archive swiper
-const swiper = new Swiper(".archive__swiper", {
-	autoplay: {
-		delay: 0,
-		desableOnInteraction: false
-	},
-	speed: 8000,
-	loop: true,
-	slidesPerView: "auto",
-	freemode: true
-});
